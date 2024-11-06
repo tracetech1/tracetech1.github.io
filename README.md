a63b1aff88b53eeec6038fe126b18b5f6c782cd2
Kavin Charlie <kavincharles@gmail.com>
2024-11-06 14:10:20 +0530

    Add

---
+++ README.md
@@ -0,0 +1,1 @@
+# TraceTechpcb.github.io
---
+++ mailing/mailfunction.php
@@ -0,0 +1,50 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require('./vendor/autoload.php');
+
+function mailfunction($mail_reciever_email, $mail_reciever_name, $mail_msg, $attachment = false){
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+    $mail->Host = "smtp.mailosaur.net";
+
+    $mail->Port = 587;
+
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+    $mail->SMTPAuth = true;
+
+    $mail->Username = "mpqyijvw@mailosaur.net";
+
+    $mail->Password = "4hs1PBgNe86SbP7VqbdaHfLtexYUuvxr";
+
+    $mail->setFrom($mail_reciever_email, $mail_reciever_name);
+
+    $mail->addAddress("TraceTechi@gmail.com", "TraceTech Team");
+
+    $mail->Subject = 'Someone Contacted You!';
+
+    $mail->isHTML($isHtml = true );
+
+    $mail->msgHTML($mail_msg);
+
+
+    if($attachment !== false){
+        $mail->AddAttachment($attachment);
+    }
+    
+    $mail->AltBody = 'This is a plain-text message body';
+ 
+    if (!$mail->send()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+?>
